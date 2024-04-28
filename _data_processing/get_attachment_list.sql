f86aca96f3d6b6c362c1f75f525052b5dd04968e
trekker <trekkerjg@gmail.com>
2024-04-28 16:09:27 +0200

    prep a lsit of attachments

---
+++ _data_processing/get_attachment_list.sql
@@ -0,0 +1,10 @@
+select
+	post_msg_id,  
+	rank() over (partition by post_msg_id order by attach_id asc) as att_order	,
+	physical_filename ,
+	real_filename ,
+	attach_comment AS comment,
+	"extension" as ext, 
+	mimetype 
+from
+    phpbb_attachments AS pa 
