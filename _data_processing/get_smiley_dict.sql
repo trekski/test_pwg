e78cf108f790b929e3c5f4e64d1ef849414eb63e
trekker <trekkerjg@gmail.com>
2024-04-29 21:02:27 +0200

    full smileys fix

--- _data_processing/get_smiley_dict.sql
+++ _data_processing/get_smiley_dict.sql
@@ -1,8 +1,10 @@
 select 
 	MAX(smiley_id) AS smiley_code,
 	smiley_url,
-	emotion 
+	emotion,
+	smiley_width as w,
+	smiley_height as h
 from 
 phpbb_smilies AS ps 
-group by 2, 3
+group by 2, 3, 4, 5
 order by 1 asc
