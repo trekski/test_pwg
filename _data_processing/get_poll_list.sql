d36d4e0cb0dc98f3816f102b0e5463a629bba0dc
trekker <trekkerjg@gmail.com>
2024-05-09 18:11:58 +0200

    polls in all threads

--- _data_processing/get_poll_list.sql
+++ _data_processing/get_poll_list.sql
@@ -1,9 +1,13 @@
 SELECT
-    topic_id,
-    poll_title
+    pt.topic_id,
+    pt.poll_title,
+    max(ppo.poll_option_total) AS max_votes
 FROM
     phpbb_topics AS pt
+    LEFT JOIN phpbb_poll_options AS ppo
+        ON pt.topic_id = ppo.topic_id 
 WHERE
     poll_title <> ''
+GROUP BY 1, 2
 ORDER BY
     topic_id ASC;
