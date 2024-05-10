037d166d5ce083badf6a2f3625064be52e5a9f1e
trekker <trekkerjg@gmail.com>
2024-05-10 16:20:04 +0200

    loop through all forums

--- _data_processing/get_forum_list.sql
+++ _data_processing/get_forum_list.sql
@@ -1,40 +1,46 @@
-with
-thread_stats as (
-    select
+WITH
+thread_stats AS (
+    SELECT
         topic_id,
-        to_timestamp(min(post_time)) as first_post
-    from
+        to_timestamp(MIN(post_time)) AS first_post
+    FROM
         phpbb_posts pp2
-    group by
+    GROUP BY
         1
 )
-,forum_stats as (
-    select
+,forum_stats AS (
+    SELECT
         pt.forum_id,
-        min(first_post) as first_post
-    from
-        phpbb_topics pt 
-        left join thread_stats as ts
-            on pt.topic_id =ts.topic_id
-    group  by 1
+        MIN(first_post) AS first_post
+    FROM
+        phpbb_topics                    AS pt 
+        LEFT join thread_stats          AS ts
+            ON pt.topic_id = ts.topic_id
+    GROUP BY 1
 )
-select
-    --f1.forum_id as grp_id,
-    --f1.forum_name as grp_name,
+SELECT
+    f1.forum_id                         AS grp_id,
+    f1.forum_name                       AS grp_name,
     f2.forum_id,
-    f2.forum_name ,
-    --f2.forum_desc ,
-    --f2.forum_topics,
-    --f2.forum_posts,
-    data_po_polsku(fs.first_post::date) as forum_first_post,
+    f2.forum_name,
+    f2.forum_desc                       AS forum_description,
+    f2.forum_topics                     AS forum_thread_count,
+    f2.forum_posts                      AS forum_post_count,
+    data_po_polsku(fs.first_post::DATE) AS forum_first_post,
     data_po_polsku(
-        to_timestamp(f2.forum_last_post_time)::date
-    ) as forum_last_post
-from
-    phpbb_forums as f1 
-    left join phpbb_forums as f2
-        on f2.parent_id = f1.forum_id
-    left join forum_stats as fs
-        on f2.forum_id = fs.forum_id
-where f1.parent_id = 0
-order by f1.forum_id asc, f2.forum_id asc
+        to_timestamp(
+            f2.forum_last_post_time
+        )::DATE
+    )                                   AS forum_last_post
+FROM
+    phpbb_forums                        AS f1 
+    LEFT JOIN phpbb_forums              AS f2
+        ON f2.parent_id = f1.forum_id
+    LEFT JOIN forum_stats               AS fs
+        ON f2.forum_id = fs.forum_id
+WHERE
+	f2.parent_id <> 0
+	AND f2.forum_id NOT IN ({exlcude_forums})
+ORDER BY
+	f1.forum_id ASC,
+	f2.forum_id ASC
